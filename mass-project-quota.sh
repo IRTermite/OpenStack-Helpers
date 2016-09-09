2a64c3f40b77a2573e4eadb08cb25a2ce91d5c1f
irtermite <irtermite@gmail.com>
2016-09-09 12:27:01 -0500

    Adding suggestions from cloudnull

--- mass-project-quota.sh
+++ mass-project-quota.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # run with ./mass-project-quota.sh ../path/to/flat/file.list
 
-while IFS='' read -r line || [[ -n "$line" ]]; do
-	project=$(echo $line | awk -F' ' '{print $1}')
-	echo "Updating quotas for tenant" $project
-	nova quota-update --cores 26 $project
-	nova quota-update --instances 13 $project
-	cinder quota-update --volumes 12 $project
-	cinder quota-update --gigabytes 500 $project
-	nova quota-update --ram 51200 $project
-	nova quota-update --sec-groups 5 $project
-	nova quota-update --floating-ips 1 $project
-done < "$1"
+#while IFS='' read -r line || [[ -n "$line" ]]; do
+for name in $(awk '{print $1}' /tmp/users); do 
+    echo "Updating quotas for tenant ${name}"
+    project="$(openstack project show "${name}" -f yaml | awk '/^id/ {print $2}')"
+    nova quota-update --cores 26 $project
+    nova quota-update --instances 13 $project
+    cinder quota-update --volumes 12 $project
+    cinder quota-update --gigabytes 500 $project
+    nova quota-update --ram 51200 $project
+    nova quota-update --security-groups 5 $project
+    nova quota-update --floating-ips 1 $project
+done
+#done < "$1"
+
